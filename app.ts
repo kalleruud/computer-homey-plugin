52992e406ecd2aeec82dde64501bc810e72395bf
kalleruud <ole@kallerud.no>
2026-04-05 15:09:25 +0200

    Refactor app initialization method to use override keyword; add @types/homey dependency in package.json and bun.lock for improved type support.

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ module.exports = class MyApp extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
-  async onInit() {
+  override async onInit() {
     this.log('MyApp has been initialized');
   }
 };
