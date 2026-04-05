353c0449c5ce1654843b5af607a987dd6f106615
kalleruud <ole@kallerud.no>
2026-04-05 15:06:12 +0200

    Update .gitignore, app.json, and package.json; add new dependencies and improve formatting in various files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import homeyApp from 'eslint-plugin-homey-app';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
   {
-    ignores: ['.homeybuild', 'node_modules', ".gitignore"],
+    ignores: ['.homeybuild', 'node_modules', '.gitignore'],
   },
   {
     files: ['**/*.{js,cjs}'],
@@ -36,10 +36,9 @@ export default [
     },
     rules: {
       ...homeyApp.configs.recommended.rules,
-      semi: ['error', 'always'],
     },
   },
-  ...tseslint.configs.recommended.map((config) => ({
+  ...tseslint.configs.recommended.map(config => ({
     ...config,
     files: ['**/*.{ts,mts,cts}'],
   })),
@@ -55,7 +54,6 @@ export default [
     },
     rules: {
       ...homeyApp.configs.recommended.rules,
-      semi: ['error', 'always'],
     },
-  },
+  }
 ];
