915197f427dce6cff1f798458cbd6848eba7a7cb
kalleruud <ole@kallerud.no>
2026-04-14 20:02:33 +0200

    Update src/computer/poweroff.ts

--- src/computer/poweroff.ts
+++ src/computer/poweroff.ts
@@ -93,7 +93,7 @@ async function shutdownComputerOverSshOnHost(
           maybeSendPassword(chunk, stream)
         })
         stream.once('close', (code: number | null | undefined) => {
-          if (code === 0 || code === null || code === undefined) {
+          if (!code) {
             finish()
             return
           }
